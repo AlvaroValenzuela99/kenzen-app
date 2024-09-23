85789ee68378e1e140e9e35419fecd2db5a37e05
Alvaro Valenzuela Gerena <alvarovgerena@gmail.com>
2024-09-23 11:23:38 +0200

    Added referrerPolicy in the iframe of ExerciseDetails component

--- components/ui/exercise-details.tsx
+++ components/ui/exercise-details.tsx
@@ -50,7 +50,8 @@ export default function ExerciseDetails({ exercise }: { exercise: Exercise }) {
                   src={exercise.video_url}
                   title={`Video demostrativo de ${exercise.exercise_name}`}
                   frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture, web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
                   allowFullScreen
                 ></iframe>
               </div>
