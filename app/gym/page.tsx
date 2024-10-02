109858478bb99e2eb951f8bc7f14862997334520
Alvaro Valenzuela Gerena <alvarovgerena@gmail.com>
2024-10-02 14:23:05 +0200

    First commit of the dev branch, changed a text in gym page to make this commit

--- app/gym/page.tsx
+++ app/gym/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
     return (
         <main>
             <h1 className="mb-4 text-xl md:text-2x1">
-                Welcome to my Kenzen Administration Dashboard
+                Bienvenido al panel de administración de tu gimnasio
             </h1>
         </main>
     )
