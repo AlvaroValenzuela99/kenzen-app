0d5a45b45c172b3591c79512600b68166bbdcac8
Alvaro Valenzuela Gerena <alvarovgerena@gmail.com>
2024-10-03 17:47:00 +0200

    Optimized import of the Athlete page

--- app/athlete/layout.tsx
+++ app/athlete/layout.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from 'next/image';
 import { signOut } from "../login/actions";
-import { Button } from "@/components/ui/button";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
